86276a07429173072307e7388b56110156dd39d3
neema-codes <neema.mwangangi@student.moringaschool.com>
2025-08-20 12:25:32 +0300

    python practice

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
-colourlist = ["red", "green", "blue"]
-print("Hello world!")
-print ("colourlist")
+age = 20
+student_names = ["Alice", "Bob", "Charlie"]
+student_names.append("Tyra")
+pet_names = ("Max", "Bella", "Bodie")
+set_of_numbers = {1, 1, 2, 2, 3, 4, 5}
+car_dict = {
+    "brand": "Toyota", 
+    "model": "Corolla", 
+    "year": 2020,
+    "color": "blue"
+}
+print("You are an adult." if age >= 18 else "You are a minor.")
+print("Student names:", student_names)
+print("Total number of students:", len(student_names))
+print("Welcome to the class!")
+print("Pet names:", pet_names)
+print("This is a pyhton set:", set_of_numbers)
+print("This is a Dictionary:", car_dict)
