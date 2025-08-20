e261f06c4ee68c81c5925606ac98968623bad29b
neema-codes <neema.mwangangi@student.moringaschool.com>
2025-08-20 11:30:58 +0300

    try it urself 1

---
+++ tiys.py
@@ -0,0 +1,10 @@
+buildings = 3
+ninjas = 25
+tunnels = 2
+samurai = 40
+total_warriors = ninjas + samurai
+print("Total warriors going into battle:", total_warriors)
+avarege_ninjas_per_building = ninjas / buildings
+avarege_samurai_per_building = samurai / buildings
+print("Average ninjas per building:", avarege_ninjas_per_building)
+print("Average samurai per building:", avarege_samurai_per_building)
